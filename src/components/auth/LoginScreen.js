a39e6b3cd0063e7abd9f83d02a325bea4c68e386
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-07 22:53:18 -0500

    feat: implementar el registro de un usario en el componente FormRegister

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormLogin } from './FormLogin';
+import { FormRegister } from './FormRegister';
 
 import './login.css';
 
@@ -14,41 +15,7 @@ export const LoginScreen = () => {
 
         <div className='col-md-6 login-form-2'>
           <h3>Registro</h3>
-          <form>
-            <div className='form-group'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Nombre'
-              />
-            </div>
-            <div className='form-group'>
-              <input
-                type='email'
-                className='form-control'
-                placeholder='Correo'
-              />
-            </div>
-            <div className='form-group'>
-              <input
-                type='password'
-                className='form-control'
-                placeholder='Contraseña'
-              />
-            </div>
-
-            <div className='form-group'>
-              <input
-                type='password'
-                className='form-control'
-                placeholder='Repita la contraseña'
-              />
-            </div>
-
-            <div className='form-group'>
-              <input type='submit' className='btnSubmit' value='Crear cuenta' />
-            </div>
-          </form>
+          <FormRegister />
         </div>
       </div>
     </div>
