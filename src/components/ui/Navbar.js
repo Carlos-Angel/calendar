8001a8ef204ba565f47d2883f04cc44cce8f6b09
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-07 23:34:14 -0500

    feat: colocar el nombre del usuario en el Navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const Navbar = () => {
+  const { name } = useSelector((state) => state.auth);
+
   return (
     <div className='navbar navbar-dark bg-dark mb-4'>
-      <span className='navbar-brand'>username</span>
+      <span className='navbar-brand'>{name}</span>
       <button className='btn btn-outline-danger'>
         <i className='fas fa-sign-out-alt' />
         <span>&nbsp;logout</span>
